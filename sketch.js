bb69e083e22108874ab16bb35a5f146227aac3b2
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-10-15 03:15:01 -0700

    rainbow

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+var c;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(0);
+  strokeWeight(2);
+}
+
+function randomColor() {
+  var a = random(0, 255);
+  var b = random(0, 255);
+  var c = random(0, 255);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed == true) {
+    var d = random(0, 255);
+ var e = random(0, 255);
+ var f = random(0, 255);
+ stroke(d,e,f);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+  }
+
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas('fileName', 'png');
+  }
+  return false;
+}
 }
