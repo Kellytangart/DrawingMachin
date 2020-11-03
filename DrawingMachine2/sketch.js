2344d4027fde2bbc5fae867a798db66abf06024e
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-11-02 20:46:57 -0800

    Final

--- DrawingMachine2/sketch.js
+++ DrawingMachine2/sketch.js
@@ -1,29 +1,47 @@
 var c;
-let noiseOffset= 0.0;
-let strokeWidth=2;
+let noiseOffset = 0.0;
+let strokeWidth = 2;
 
 function setup() {
   createCanvas(600, 600);
   background(0);
+  strokeWeight(2);
 }
 
-function randomColor() {
-  var a = random(0, 255);
-  var b = random(0, 255);
-  var c = random(0, 255);
-}
 
 function draw() {
-    strokeWeight(strokeWidth);
-  noiseOffset+=0.05;
-  strokeWidth= noise(noiseOffset) * 20;
+  strokeWeight(strokeWidth);
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 20;
 
   if (mouseIsPressed == true) {
     var d = random(0, 255);
     var e = random(0, 255);
     var f = random(0, 255);
     stroke(d, e, f);
-    line(mouseX, mouseY, pmouseX, pmouseY);}
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+  }
+
+  if (keyIsPressed === true) { //eraser tool - hold any key and drag mouse around to use eraser
+    noStroke();
+    fill('black');
+    ellipse(mouseX, mouseY, 30, 30);
+    x = mouseY;
+    y = mouseX;
+  }
+
+  function keyTyped() {
+    if (key === 's') {
+      saveCanvas('fileName', 'png');
+      var d = random(0, 255);
+      var e = random(0, 255);
+      var f = random(0, 255);
+      stroke(d, e, f);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+  }
+
   function keyTyped() {
     if (key === 's') {
       //save the image
--- sketch.js
+++ sketch.js
@@ -1,66 +1,53 @@
-var c;
-let noiseOffset = 0.0;
-let strokeWidth = 2;
-
+var hue;
+var rainbow = true;
+var words = ["Press r for a topic","Tamagotchi", "Mighty Beans", "Ipod", "Tech Deck", "Idog", "ZhuZhu Pets","Beyblade","Aquapets"];
+var index = 0
 function setup() {
-  createCanvas(600, 600);
-  //background(0);
-  drawGrid();
+  createCanvas(890,600);
+  background(0);
+  strokeWeight(2);
+  hue = 0;
+  cursor ('tool.png')
 }
 
-function randomColor() {
-  var a = random(0, 255);
-  var b = random(0, 255);
-  var c = random(0, 255);
+function draw(){
+cursor ('tool.png')
+fill('pink');
+textSize(22);
+textFont("Comic Sans MS");
+    text(words[index], 25, 25);
 }
-
-function draw() {
-  strokeWeight(strokeWidth);
-  noiseOffset += 0.05;
-  strokeWidth = noise(noiseOffset) * 20;
-}
-
-if (mouseIsPressed == true) {
-  var d = random(0, 255);
-  var e = random(0, 255);
-  var f = random(0, 255);
-  stroke(d, e, f);
+function mouseDragged() {
+  if (rainbow) {
+    if (hue > 360) {
+        hue = 0;
+    } else {
+        hue++;
+    }
+	}
+  colorMode(HSL, 360);
+  stroke(hue, 200, 200);
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
-
-function keyTyped() {
-  if (key === 's') {
-    //save the image
-    saveCanvas('fileName', 'png');
-  } else if (key === 'c') {
-    //clear the image
-    clear();
-  } else if (key === 'e') {
-    //eraser tool
-    noStroke();
-    fill('black');
-    ellipse(mouseX, mouseY, 50, 50);
-    x = mouseY;
-    y = mouseX;
-  }
-  return false;
-}
-
-function drawGrid() {
-  clear();
-  numCells = 20;
-  fillColor = 255;
-  stroke(0);
-  for (let i = 0; i <= width; i += width / numCells) {
-    for (let j = 0; j <= height; j += height / numCells) {
-      if (fill === 255) {
-        fillColor = 200;
-      } else {
-        fillColor = 255;
-      }
-      fill(fillColor);
-      rect(i, j, width / numCells, height / numCells);
-
-    }
-  }
-}
+ function keyTyped() {
+   if (key === 's') {
+     //save the image
+     saveCanvas('fileName', 'png');
+   } else if (key === 'c') {
+     //clear the image
+     clear();
+     background(0);
+   } else if (key === 'o') {
+     //eraser tool
+     noStroke();
+     fill('black');
+     ellipse(mouseX, mouseY, 50, 50);
+     x = mouseY;
+     y = mouseX;
+   }else if (key === 'r') {
+        index = Math.floor(random(0, words.length));
+        clear();
+        background(0);
+   }
+   return false;
+ }
